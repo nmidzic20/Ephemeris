78d9fb974d51047c3b65af52d467df0db9a23a30
Noa Midzic <nmidzic@yahoo.com>
2023-05-31 19:36:50 +0200

    Added video player since DailyImage can also return media_type video

---
+++ app/src/main/java/endava/astrolab/app/ui/component/VideoPlayer.kt
@@ -0,0 +1,36 @@
+package endava.astrolab.app.ui.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
+@Composable
+fun YoutubeScreen(
+    videoId: String,
+    modifier: Modifier = Modifier
+) {
+    AndroidView(factory = {
+        var view = YouTubePlayerView(it)
+        view.addYouTubePlayerListener(
+            object : AbstractYouTubePlayerListener() {
+                override fun onReady(youTubePlayer: YouTubePlayer) {
+                    super.onReady(youTubePlayer)
+                    youTubePlayer.loadVideo(videoId, 0f)
+                }
+            }
+        )
+        view
+    })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    YoutubeScreen(videoId = "X4UF9Akman0")
+}
